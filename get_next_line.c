393d8d87851554d5b37372d4175562637935959c
Georges SYSAATH <gsysaath@e1r5p23.42.fr>
2017-12-20 04:24:07 +0100

    hehe

---
+++ get_next_line.c
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gsysaath <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/12/20 01:54:28 by gsysaath          #+#    #+#             */
+/*   Updated: 2017/12/20 04:21:05 by gsysaath         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include "get_next_line.h"
+#include <stdio.h>
+
+char		*beforebn(char *buf, char *tmp)
+{
+	int		i;
+	char	*s1;
+	char	*s2;
+
+	i = 0;
+	s1 = ft_strdup(buf);
+	s2 = ft_strdup(tmp);
+	while (s2[i] != '\n')
+		i++;
+	s2[i] = '\0';
+	return (ft_strjoin(s1, s2));
+}
+
+static char			*bntmp(char **line, char *buf, char *tmp)
+{
+	*line = beforebn(buf, tmp);
+	buf = ft_strdup(ft_strchr(tmp, '\n'));
+	return (buf);
+}
+
+static char			*bnbuf(char **line, char *buf)
+{
+	*line = beforebn("", buf);
+	buf = ft_strdup(ft_strchr(buf, '\n'));
+	return (buf);
+}
+
+int			get_next_line(const int fd, char **line)
+{
+	static char		*buf = "";
+	char			tmp[BUFF_SIZE + 1];
+
+	if (fd == -1 || BUFF_SIZE < 1 || read(fd, "", 0))
+		return (-1);
+	while ((read(fd, tmp, BUFF_SIZE)) != 0)
+	{
+		printf("ici-----[%s]\n", buf);
+		if (!(ft_strchr(tmp, '\n') == NULL))
+		{
+			buf = bntmp(line, buf, tmp);
+			return (1);
+		}
+		buf = ft_strjoin(buf, tmp);
+	}
+	if (buf != NULL && *buf != '\0')
+	{
+		printf("je suis la [%s]\n", buf);
+		if (ft_strchr(buf, '\n') != NULL)
+		{
+			buf = bnbuf(line, buf);
+			return (1);
+		}
+		*line = ft_strdup(buf);
+		free(buf);
+		return (1);
+	}
+	return (0);
+}
